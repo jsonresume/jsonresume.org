7da4b862a2861f316e89c2badb944c728172723b
Thomas Davis <thomasalwyndavis@gmail.com>
2023-08-25 23:09:33 +1000

    wip

--- apps/registry/pages/api/formatters/qr.js
+++ apps/registry/pages/api/formatters/qr.js
@@ -1,19 +1,19 @@
-import qr from 'qr-image';
+import qr from "qr-image";
 
 export const format = async function format(resume, { username }) {
-  const code = qr.image('https://registry.jsonresume.org/' + username, {
-    type: 'png',
-    ec_level: 'S',
+  const code = qr.image("https://registry.jsonresume.org/" + username, {
+    type: "png",
+    ec_level: "S",
     size: 60,
     margin: 1,
   });
-  console.log({ code });
+
   return {
     content: code,
     headers: [
       {
-        key: 'Content-Type',
-        value: 'image/png',
+        key: "Content-Type",
+        value: "image/png",
       },
     ],
   };
