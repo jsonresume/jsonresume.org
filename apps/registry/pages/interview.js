a68994be3d630627401135159700fa821445faa7
Ajax Davis <thomasalwyndavis@gmail.com>
2023-09-26 12:14:24 +1000

    removed component

--- apps/registry/pages/interview.js
+++ apps/registry/pages/interview.js
@@ -10,18 +10,6 @@ function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-const InputContainer = styled.div`
-  position: fixed;
-  background: #fff18f;
-  bottom: 0;
-  left: 0;
-  width: 100vw;
-  height: 120px;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-`;
 
 const Input = styled.input`
   bottom: 50px;
