73800339c770bbfe4b4456b01842a9a9fd9ed0b7
Thomas Davis <thomasalwyndavis@gmail.com>
2024-05-09 20:17:04 +1000

    allow cors for all dynamic resume route request

--- apps/registry/pages/api/formatters/json.js
+++ apps/registry/pages/api/formatters/json.js
@@ -1,13 +1,7 @@
 const format = async function format(resume) {
   return {
     content: JSON.stringify(resume, undefined, 4),
-    headers: [
-      {
-        'Access-Control-Allow-Headers':
-          'Origin, X-Requested-With, Content-Type, Accept',
-      },
-      { 'Access-Control-Allow-Origin': '*' },
-    ],
+    headers: [],
   };
 };
 
