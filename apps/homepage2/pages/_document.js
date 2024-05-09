9e9e60d7898c5cf5c57d56a5dba0f955548a6df0
Thomas Davis <thomasalwyndavis@gmail.com>
2024-05-09 19:29:54 +1000

    readded clicky tracking

--- apps/homepage2/pages/_document.js
+++ apps/homepage2/pages/_document.js
@@ -12,6 +12,21 @@ export default class MyDocument extends Document {
           <link rel="dns-prefetch" href="//fonts.googleapis.com" />
           <link rel="dns-prefetch" href="//cdnjs.cloudflare.com" />
           <link rel="dns-prefetch" href="//static.getclicky.com" />
+          <noscript>
+            <p>
+              <img
+                alt="Clicky"
+                width="1"
+                height="1"
+                src="//in.getclicky.com/101412017ns.gif"
+              />
+            </p>
+          </noscript>
+          <script
+            async
+            data-id="101412017"
+            src="//static.getclicky.com/js"
+          ></script>
 
           <meta charset="utf-8" />
           <meta http-equiv="X-UA-Compatible" content="IE=edge" />
