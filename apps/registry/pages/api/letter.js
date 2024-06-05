9bd1a67256ba0bc33fdb0a8fd469567b9925e9f6
Thomas Davis <thomasalwyndavis@gmail.com>
2024-06-06 09:18:36 +1000

    refactor letter api (#104)

--- apps/registry/pages/api/letter.js
+++ apps/registry/pages/api/letter.js
@@ -4,24 +4,16 @@ const { createClient } = require('@supabase/supabase-js');
 const supabaseUrl = 'https://itxuhvvwryeuzuyihpkp.supabase.co';
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
+
 /*
-#todo
- - add an input box to post the job description
-  - save JD in local storage
- - buttons that can change tone of the letter
-  - save settings in local storage
-
-
-
-
 #wishlist
- - google the company, and include as much up to date information as possible in the prompt
-
+ - Google the company, and include as much up to date information as possible in the prompt
 */
 
 export default async function handler(req, res) {
   const { username, jobDescription, tone } = req.body;
 
+  // pull from the cache
   const { data } = await supabase
     .from('resumes')
     .select()
@@ -31,7 +23,7 @@ export default async function handler(req, res) {
 
   const resumeString = JSON.stringify(resume);
 
-  const api = new ChatGPTAPI({
+  const gpt = new ChatGPTAPI({
     apiKey: process.env.OPENAI_API_KEY,
     model: 'gpt-4o',
     completionParams: {
@@ -41,9 +33,9 @@ export default async function handler(req, res) {
 
   let prompt = [
     `
-You are a human candidate for a job. Read the supplied resume and pretend you are that person. Your resume has been supplied to them.
+  You are a human candidate for a job. Read the supplied resume and pretend you are that person. Your resume has been supplied to them.
 
-This is your resume in the JSON format. Reference it for the cover letter.
+  This is your resume in the JSON format. Reference it for the cover letter.
 
   ${resumeString}
 
@@ -52,24 +44,18 @@ This is your resume in the JSON format. Reference it for the cover letter.
 
   if (jobDescription) {
     prompt.push(`
-
     Here is the job description I am applying for;
      ${jobDescription}.
-
     `);
   }
 
   prompt.push(
-    `Using a ${tone} tonality. Format your response using Markdown. Don't be afraid to name your lack of experience the candidate might have but focus on your strengths. Keep it two a couple short paragraphs only 300 words.`
+    `Using a ${tone} tonality. Format your response using Markdown. Don't be afraid to name your lack of experience the candidate might have but focus on your strengths. Keep it two a couple short paragraphs only 300 words.
+
+    Please write a short cover letter. Make sure you write a cover letter.`
   );
 
-  prompt.push(
-    'Please write a short cover letter. Make sure you write a cover letter.'
-  );
+  const gptRes = await gpt.sendMessage(prompt.join(''));
 
-  console.log({ prompt });
-
-  const res2 = await api.sendMessage(prompt.join(''));
-
-  return res.status(200).send(res2.text);
+  return res.status(200).send(gptRes.text);
 }
