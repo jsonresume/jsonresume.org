7479d46532d9ec3699979afd35052a36c0fa0d54
Ajax Davis <thomasalwyndavis@gmail.com>
2024-06-15 06:01:27 +1000

    fix lint errors

--- apps/registry/lib/formatters/rendercv.js
+++ apps/registry/lib/formatters/rendercv.js
@@ -1,17 +1,4 @@
-const YAML = require('json-to-pretty-yaml');
 import convert from '../../../../packages/converters/jsonresume-to-rendercv/convert';
-
-const networks = {
-  facebook: 'Facebook',
-  github: 'GitHub',
-  gitlab: 'GitLab',
-  instagram: 'Instagram',
-  linkedin: 'LinkedIn',
-  medium: 'Medium',
-  stackoverflow: 'StackOverflow',
-  twitter: 'Twitter',
-  youtube: 'YouTube',
-};
 
 const format = async function format(jsonResume) {
   const rendercv = convert(jsonResume);
