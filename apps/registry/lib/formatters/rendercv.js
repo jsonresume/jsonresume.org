9e9bbf845814c63051d644581ba2dd3f88090d24
Thomas Davis <thomasalwyndavis@gmail.com>
2024-06-16 00:52:27 +1000

    added rendercv to getting started (#109)

--- apps/registry/lib/formatters/rendercv.js
+++ apps/registry/lib/formatters/rendercv.js
@@ -1,7 +1,7 @@
-import convert from '../../../../packages/converters/jsonresume-to-rendercv/convert';
+import { convert } from '../../../../packages/converters/jsonresume-to-rendercv/convert';
 
-const format = async function format(jsonResume) {
-  const rendercv = convert(jsonResume);
+const format = async function format(resume) {
+  const rendercv = await convert(resume);
   return { content: rendercv, headers: [] };
 };
 
