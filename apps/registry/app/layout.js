9df240e17c3016404e757ad7eb6c302051372ade
Ajax Davis <thomasalwyndavis@gmail.com>
2024-06-15 20:58:31 +1000

    fix viewport

--- apps/registry/app/layout.js
+++ apps/registry/app/layout.js
@@ -4,6 +4,11 @@ import { signOut } from 'next-auth/react';
 export const metadata = {
   title: 'JSON Resume Registry',
 };
+
+export const viewport = {
+  initialScale: 1,
+  width: 'device-width',
+};
 export default function Layout({ children }) {
   return (
     <html lang="en">
@@ -21,8 +26,6 @@ export default function Layout({ children }) {
           }
         />
         {children}
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-
         <link
           href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap"
           rel="stylesheet"
