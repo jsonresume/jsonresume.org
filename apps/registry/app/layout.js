ca345742a6cba6e17335932e57b0d06299890c17
Ajax Davis <thomasalwyndavis@gmail.com>
2024-06-15 20:48:39 +1000

    lint

--- apps/registry/app/layout.js
+++ apps/registry/app/layout.js
@@ -1,22 +1,12 @@
 import Header from '@jsonresume/ui/Header';
-import Head from 'next/head';
 import { signOut } from 'next-auth/react';
 
+export const metadata = {
+  title: 'JSON Resume Registry',
+};
 export default function Layout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>JSON Resume Registry</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link
-          href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,300,600,800&display=swap"
-        />
-      </Head>
       <body>
         <Header
           left={
@@ -31,6 +21,16 @@ export default function Layout({ children }) {
           }
         />
         {children}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link
+          href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,300,600,800&display=swap"
+        />
       </body>
     </html>
   );
