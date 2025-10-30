2100645a1779c55323684c3a2d3449fe6a8b4cfb
Ajax Davis <thomasalwyndavis@gmail.com>
2025-10-31 02:36:10 +1000

    fix: update debug page to use reference theme instead of removed standard theme

--- apps/registry/pages/debug.js
+++ apps/registry/pages/debug.js
@@ -1,4 +1,4 @@
-import { Resume } from 'jsonresume-theme-standard';
+import { Resume } from 'jsonresume-theme-reference';
 import resume from './api/samples/resume';
 export default function Talk() {
   // @todo - used to render react templates (purely debug stuff at the moment)
